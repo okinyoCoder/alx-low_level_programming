93ec097a6eeb7253f46d61cb4ec68bec640bec90
okinyoCoder <okinyoanthony@gmail.com>
2022-06-05 11:50:51 +0000

    program that prints the alphabet in lowercase, and then in uppercase using putchar function, followed by a new line

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
 
 /**
- * main - prints a to z using putchar function
+ * main - prints alphabet in lowercase, and then in uppercase
+ * followed by a new line
  * Return: returns zero
  */
 int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= z; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'A'; i <= z; i++)
 	{
 		putchar(i);
 	}
