12e787999e1cdf7667f5ffbeda8c7f1e228c83bd
okinyoCoder <okinyoanthony@gmail.com>
2022-06-06 18:11:03 +0000

    program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+/**
+ * main - program that prints all possible different combinations of two
+ * digits Numbers must be separated by ,, followed by a space
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
