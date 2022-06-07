fe15603216edda0ac6ca0f81dcd159e1c744e51e
okinyoCoder <okinyoanthony@gmail.com>
2022-06-07 20:16:02 +0000

    added _putchar and main function prototype

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+/*
+ * File: main.h
+ * Auth: Odhiambo
+ * Desc: Header file containing declaration for all functions used
+ * 	 in the 0x02ii-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+int main(void);
+
+#endif
