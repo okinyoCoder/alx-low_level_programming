139825ff680972292e2ce2577010796aea757cff
okinyoCoder <okinyoanthony@gmail.com>
2022-06-09 19:01:16 +0000

    added _putchar prototype function to main.h

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Continuation of functions, more nested loops at ALX
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+/**
+ * File: main.h
+ * Auth: okinyo
+ * Desc: Header file containing declaration for all functions used
+ * 	 in the 0x02ii-functions_nested_loops directory
+ */
+#endif 
+
+int _putchar(char c);
