f3ddebca7e808f179043e58f848b53ee005e55fd
okinyoCoder <okinyoanthony@gmail.com>
2022-06-09 20:22:49 +0000

    function that prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line
  *
- * Return: 0 when success
  */
 void print_numbers(void)
 {
@@ -16,5 +15,4 @@ void print_numbers(void)
 		c++;
 	}
 	putchar('\n');
-	return (0);
 }
