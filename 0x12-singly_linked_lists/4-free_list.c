a7c7a68b2f5aa082cf41bf2a78d12fb995a49226
okinyoCoder <okinyoanthony@gmail.com>
2022-07-14 20:48:28 +0000

    tion that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list.
+ * @head: a list that enters to the function.
+ */
+void free_list(list_t *head)
+{
+list_t *temp;
+
+	temp = head;
+	while (temp != NULL)
+	{
+		free(temp->str);
+		free(temp);
+		temp = temp->next;
+	}
+}
