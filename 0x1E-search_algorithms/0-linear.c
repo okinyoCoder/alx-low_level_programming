cd60022267a792027a43614fe834faa38303b85e
okinyoCoder <okinyoanthony@gmail.com>
2023-05-10 14:26:48 +0000

    updated 0-linear.c file

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,16 +10,23 @@
  *
  */
 
-int linear_search(int *array, size_t size, int value)?{
-	size_t i = 0;
-	if ( array == NULL || size == 0){
-		return (-1);
-	}
-	for (i = 0; i < size; i++)){
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			break;
-	}
+int linear_search(int *array, size_t size, int value)
+{
+		size_t i;
 
-	return (i);
-}
+		if (array == NULL)
+		{
+				return (-1);
+				}
+		for (i = 0; i < size; i++)
+		{
+				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+						break;
+						}
+		if (i == size)
+		{
+				return (-1);
+				}
+		return (i);
+		}
